31740107bae253490dbda947f0b47586ec1096c3
comfuture <comfuture@iplateia.com>
2022-10-17 02:12:45 +0000

    simplify instance link name

--- edgedb_link.sh
+++ edgedb_link.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# try link to edgedb docker instance as name "dev_local"
+# try link to edgedb docker instance as name "dev"
 echo edgedb | edgedb instance link \
   --overwrite \
   --dsn edgedb://edgedb:edgedb@edgedb:5656 \
   --password-from-stdin \
   --non-interactive \
   --trust-tls-cert \
-  dev_local
+  dev
